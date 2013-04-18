ebe12ce85c770ba02ae9aec6ab1fde08c6e6b8a3
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2013-04-17 20:36:43 -0500

    Use cemerick.piggieback to hook CLJS REPL to nREPL

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.cemerick/piggieback "0.0.4"]
                  [compojure "1.1.5"]]
+
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]} 
   
   :plugins [[lein-cljsbuild "0.3.0"]
             [lein-ring "0.8.3"]]
--- src/cljs/tankz/connect.cljs
+++ src/cljs/tankz/connect.cljs
@@ -1,4 +1,10 @@
 (ns tankz.connect
   (:require [clojure.browser.repl :as repl]))
 
+;; To connect using `lein repl`:
+;; user=> (require 'cljs.repl.browser)
+;; user=> (cemerick.piggieback/cljs-repl
+;;  #_=>   :repl-env (doto (cljs.repl.browser/repl-env :port 9000)
+;;  #_=>   cljs.repl/-setup))
+
 (repl/connect "http://localhost:9000/repl")
