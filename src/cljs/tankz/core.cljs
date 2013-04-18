51425274f2ec0f4d77cc0403c933b5143c3118bf
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2013-04-17 19:53:46 -0500

    Add asset loading

--- src/cljs/tankz/core.cljs
+++ src/cljs/tankz/core.cljs
@@ -2,6 +2,26 @@
 
 (def colors ["orange" "blue" "red" "green" "yellow" "magenta" "cyan"])
 
+(defn log [x & xs]
+  (.log js/console (apply str x xs)))
+
+(defn load-img [name]
+  (let [img (js/Image)
+        src (str "/img/" name)]
+    (log "Loading:" name)
+    (set! (.-src img) src)
+    img))
+
+(defn load-images []
+  {:tank (load-img "tank.png")
+   :turrent (load-img "turret.png")
+   :sand (load-img "sand.png")})
+
+(defn new-game []
+  {:assets (load-images)
+   ; ...
+   })
+
 (defn request-frame [callback]
   ((or (.-requestAnimationFrame js/window)
       (.-webkitRequestAnimationFrame js/window)
@@ -11,14 +31,20 @@
       (fn [callback] (.setTimeout js/window callback (/ 1000 60))))
      callback))
 
-(defn animate []
+(defn draw-game [game]
   (let [canvas (.getElementById js/document "game")
         context (.getContext canvas "2d")]
-    (set! (.-fillStyle context) (rand-nth colors))
-    (.fillRect context 0 0 (.-width canvas) (.-height canvas))
-    (request-frame animate)))
+    (doseq [x (range 0 (.-width canvas) 32)
+          y (range 0 (.-height canvas) 32)]
+      (.drawImage context (:sand (:assets game)) x y))))
+
+(defn game-loop [game]
+  (if (reduce (fn [acc [_ v]] (and acc (.-complete v))) (:assets game))
+    (draw-game game)
+    (log "Not Ready"))
+  (request-frame #(game-loop game)))
 
 (defn ^:export init []
   (if (and js/document
            (.-getElementById js/document))
-    (animate)))
+    (game-loop (new-game))))
