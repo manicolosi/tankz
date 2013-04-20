3ccab3149618bb9f2d23380208373d39b0bf1e83
Mark A. Nicolosi <mark.a.nicolosi@gmail.com>
2013-04-20 10:16:49 -0500

    Fix instructions for browser REPL to nREPL

--- src/cljs/tankz/connect.cljs
+++ src/cljs/tankz/connect.cljs
@@ -1,10 +1,11 @@
 (ns tankz.connect
   (:require [clojure.browser.repl :as repl]))
 
-;; To connect using `lein repl`:
-;; user=> (require 'cljs.repl.browser)
-;; user=> (cemerick.piggieback/cljs-repl
-;;  #_=>   :repl-env (doto (cljs.repl.browser/repl-env :port 9000)
-;;  #_=>   cljs.repl/-setup))
-
+;; Connect with `lein trampoline cljsbuild repl-listen`
 (repl/connect "http://localhost:9000/repl")
+
+;; Run after `lein repl` to connect with nREPL
+(comment
+  (require 'cljs.repl.browser)
+  (cemerick.piggieback/cljs-repl
+    :repl-env (doto (cljs.repl.browser/repl-env :port 9000) cljs.repl/-setup)))
